bfc1b24e5e158a6fcbe7484a7746f868f27bdd42
Jose Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-05-20 23:32:40 -0600

    actions types creado

---
+++ ClinicManagementSystem.Frontend/src/auth/actions/types.js
@@ -0,0 +1,6 @@
+export const INITIALIZE = 'INITIALIZE';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const LOGOUT = 'LOGOUT';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const REFRESH_TOKEN = 'REFRESH_TOKEN';
