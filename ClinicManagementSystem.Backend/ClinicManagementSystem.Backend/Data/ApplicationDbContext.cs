418709010e381468c672f5f8a96c5e31e77fc1fd
Jose Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-06-03 19:17:32 -0600

    IdentityDbContext agregado

---
+++ ClinicManagementSystem.Backend/ClinicManagementSystem.Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ClinicManagementSystem.Backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicManagementSystem.Backend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
