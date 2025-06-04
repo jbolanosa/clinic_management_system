fa48da4ea1a4788780bc09aaf38ea2f62d81cd0f
Jose Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-06-03 19:16:49 -0600

    generarToken y refreshtoken agregado

---
+++ ClinicManagementSystem.Backend/ClinicManagementSystem.Backend/Controllers/AuthController.cs
@@ -0,0 +1,174 @@
+﻿using ClinicManagementSystem.Backend.DTOs;
+using ClinicManagementSystem.Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ClinicManagementSystem.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO registerDto)
+        {
+            if (registerDto is null)
+            {
+                return BadRequest("Invalid registration data.");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = registerDto.Email,
+                Email = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            // Logic for user registration
+            return Ok(new AuthReponseDTO() { Status = "Success", Message = "The user has been successfully registered." });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDto)
+        {
+            // Logic for user login
+            if (loginDto is null)
+            {
+                return BadRequest("Invalid login data.");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = _userManager.FindByEmailAsync(loginDto.Email).Result;
+            if (user is null)
+            {
+                return Unauthorized("Invalid email or password.");
+            }
+            var result = _userManager.CheckPasswordAsync(user, loginDto.Password).Result;
+            if (!result)
+            {
+                return Unauthorized("Invalid email or password.");
+            }
+            var token = GenerateJwtToken(user);
+            var refreshToken = GenerateRefreshToken();
+            user.RefreshToken = refreshToken; // Assuming ApplicationUser has a RefreshToken property
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(Convert.ToDouble(_configuration["Jwt:RefreshTokenValidityInDays"])); // Set the expiry time for the refresh token
+            await _userManager.UpdateAsync(user); // Update the user with the new refresh token
+            // Return the JWT token and refresh token
+
+            return Ok(new AuthReponseDTO { Status = "Success", Token = token, RefreshToken = refreshToken, Message = "Login successful." });
+        }
+
+        [Authorize]
+        [HttpPost("refreshToken")]
+        public async Task<IActionResult> RefreshToken([FromBody] TokenDTO tokenDto)
+        {
+            var principal = GetPrincipalFromExpiredToken(tokenDto.Token);
+            var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+            if (userId == null)
+                return BadRequest("Token inválido");
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null || user.RefreshToken != tokenDto.RefreshToken || user.RefreshTokenExpiryTime <= DateTime.UtcNow)
+                return Unauthorized();
+
+            var newAccessToken = GenerateJwtToken(user);
+            var newRefreshToken = GenerateRefreshToken();
+
+            user.RefreshToken = newRefreshToken;
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(Convert.ToDouble(_configuration["Jwt:RefreshTokenExpireDays"]));
+            await _userManager.UpdateAsync(user);
+
+            // Logic for refreshing the token
+            return Ok(new AuthReponseDTO() { Status = "Success", Message = "Generado nuevo token", Token = newAccessToken, RefreshToken = newRefreshToken});
+        }
+
+
+        public string GenerateJwtToken(ApplicationUser user)
+        {
+            // Logic for generating a JWT token
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Email, user.Email!) // Fix: Added 'user.Id' as the second argument
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["TokenValidityInMinutes"]!)),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Audience = jwtSettings["ValidAudience"],
+                Issuer = jwtSettings["ValidIssuer"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token); // Return the generated token
+        }
+
+        public string GenerateRefreshToken()
+        {
+            return Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)); // Generate a random refresh token
+        }
+
+        private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = false,
+                ValidIssuer = jwtSettings["ValidIssuer"],
+                ValidAudience = jwtSettings["ValidAudience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Token inválido");
+
+            return principal;
+        }
+    }
+}
